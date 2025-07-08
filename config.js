d15244fac98c53772d24ae99fb93fc45752ffb78
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2025-07-08 19:05:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ module.exports = {
   ALIVE_MSG: process.env.ALIVE_MSG || "*🌀ONYX MOVIE BOT🎞BY AROSH🌀*\n\n--------I am alive now...!👻-------\n\n> With this ONYX MOVIE DOWNLOADER BOT you can download movies via Whatsapp 🔥▶\n> මේ ONYX MOVIE DOWNLOADER BOT මගින් ඔබට Whatsapp හරහා Movie Download කර ගැනීමෙ හැකියාව ඇත 🔥▶\n> இந்த ONYX MOVIE DOWNLOADER BOT மூலம் நீங்கள் Whatsapp வழியாக திரைப்படங்களை பதிவிறக்கம் செய்யலாம் 🔥▶\n\n✅ Github repository = https://github.com/aroshsamuditha/ONYX-MOVIE-BOT\n✅Youtube = https://www.youtube.com/@ONYXSTUDIO2005\n✅ Tiktok Page = https://www.tiktok.com/@onyxstudio_byarosh?_t=ZS-8xQGlXXfj3o&_r=1\n\n> By Arosh Samuditha",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE: process.env.MODE || "public",
-  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|704cd20313ac2a28458fb6f968ec5f6f4d6aa2f5", //https://api.skymansion.site/movies-dl/
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|2ed9f867b36aa2e04701712ae43f2f519b21f7fd", //https://api.skymansion.site/movies-dl/
 };
